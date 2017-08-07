aa713635acb68ebf5212636e442d42cdb8162ce5
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-06 21:20:44 -0300

    adicionada a variável de ambiente CASSANDRA_KEYSPACE

--- docker-run.sh
+++ docker-run.sh
@@ -2,8 +2,9 @@ docker run -d \
     --name kafka2cassandra \
     --net bigdata \
     --ip 172.18.0.6 \
-    --env KAFKA_BOOTSTRAP_SERVERS="172.17.0.4:9092" \
+    --env KAFKA_BOOTSTRAP_SERVERS="172.18.0.4:9092" \
     --env KAFKA_TOPIC="tweets" \
-    --env CASSANDRA_ENDPOINT="172.17.0.2:9042" \
+    --env CASSANDRA_ENDPOINT="172.18.0.2" \
+    --env CASSANDRA_KEYSPACE="tweets" \
     infobarbosa/kafka2cassandra:1.0-SNAPSHOT
 
